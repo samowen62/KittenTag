1cd11afa35f59c7cc2f593bb732135356d7d3676
Sam <samowen62@yahoo.com>
2017-01-07 18:59:27 -0600

    Creating repository

---
+++ Assets/Scripts/Enums/DirectionEnum.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public enum DirectionEnum : int{
+    NORTH = 0,
+    EAST = 1,
+    SOUTH = 2,
+    WEST = 3,
+    IDLE = -1
+}
---
+++ Assets/Scripts/KittenController.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KittenController : MonoBehaviour {
+
+    private Animator animator;
+
+    private const string DIRECTION = "Direction";
+    private DirectionEnum _currentDirection = DirectionEnum.IDLE;
+    private bool _current_is_left = false;
+
+    // Use this for initialization
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        var vertical = Input.GetAxis("Vertical");
+        var horizontal = Input.GetAxis("Horizontal");
+
+        if (vertical > 0)
+        {
+            animator.SetInteger(DIRECTION, (int)DirectionEnum.SOUTH);
+        }
+        else if (vertical < 0)
+        {
+            animator.SetInteger(DIRECTION, (int)DirectionEnum.NORTH);
+        }
+        else if (horizontal > 0)
+        {
+            animator.SetInteger(DIRECTION, (int)DirectionEnum.EAST);
+            flipDirection(true);
+        }
+        else if (horizontal < 0)
+        {
+            animator.SetInteger(DIRECTION, (int)DirectionEnum.WEST);
+            flipDirection(false);
+        }
+        else
+        {
+            //TODO: for testing make a universal idle state so we know. (use the box lel)
+            animator.SetInteger(DIRECTION, -1);
+        }
+    }
+
+    private void flipDirection(bool isLeft)
+    {
+
+        if (_current_is_left ^ isLeft)
+        {
+            if (isLeft)
+            {
+                transform.Rotate(0, 180, 0);
+            }
+            else
+            {
+                transform.Rotate(0, -180, 0);
+            }
+            _current_is_left = isLeft;
+        }
+
+    }
+}
